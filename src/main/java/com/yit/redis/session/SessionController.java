abf0beb2dde9039ceaa3c8a145ab93719de0cdd7
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2020-08-21 14:40:12 +0700

    first submit

---
+++ src/main/java/com/yit/redis/session/SessionController.java
@@ -0,0 +1,12 @@
+package com.yit.redis.session;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionController {
+    @RequestMapping("/")
+    public String helloAdmin() {
+        return "Hello Admin";
+    }
+}
