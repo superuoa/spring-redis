45c01fc3b44cbe6108c4ca6c2ac561decd7652e3
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2020-08-21 15:55:00 +0700

    update password endcoder

--- src/main/java/com/yit/redis/security/SecurityConfig.java
+++ src/main/java/com/yit/redis/security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.yit.redis.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,9 +16,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+    	String password = passwordEncoder().encode("myRadisP@ss");
+    	auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+            .withUser("admin").password("password").roles("ADMIN");
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic()
